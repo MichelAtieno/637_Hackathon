48fa5d0bf0b5238108e406704eb4da662ff1e7a3
Michel Atieno <michelatieno23@gmail.com>
2023-11-24 17:52:24 +0300

    Add fiall files

---
+++ hackathon.py
@@ -0,0 +1,17 @@
+from pyresparser import ResumeParser
+import os
+from docx import Document
+
+filed="Michel Atieno Software Engineer Intern Curriculum Vitae.pdf"
+print(filed)
+try:
+    doc = Document()
+    with open(filed, 'r', encoding="utf-8") as file:
+        doc.add_paragraph(file.read())
+        print(filed)
+    doc.save("text.docx")
+    data = ResumeParser('text.docx').get_extracted_data()
+    print(data['skills'])
+except:
+    data = ResumeParser(filed).get_extracted_data()
+    print(data['skills'])
